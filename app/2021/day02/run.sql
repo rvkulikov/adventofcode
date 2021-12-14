d6e6f99982406c025ae94274b71e3f4c66ac7df2
Roman Kulikov <r.v.kulikov@yandex.ru>
2021-12-15 02:52:12 +0300

    day02 part1

---
+++ app/2021/day02/run.sql
@@ -0,0 +1,30 @@
+drop schema if exists day02 cascade;
+create schema day02;
+
+create table day02.input (
+  movement_id serial,
+  movement_direction text,
+  movement_distance int
+);
+
+\copy day02.input (movement_direction, movement_distance) from '2021/day02/input.csv' with (delimiter ' ');
+
+with
+  deltas as (
+    select
+      case
+        when movement_direction = 'forward' then movement_distance
+        else 0
+        end as horizontal,
+      case
+        when movement_direction = 'up' then -movement_distance
+        when movement_direction = 'down' then movement_distance
+        else 0
+        end as vertical
+    from
+      day02.input
+  )
+select
+  format('Part 1 answer is: %s', sum(horizontal) * sum(vertical))
+from
+  deltas
