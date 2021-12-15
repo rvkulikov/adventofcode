319099879d6764d91f2360d0242dbe4aea427320
Roman Kulikov <r.v.kulikov@yandex.ru>
2021-12-15 03:04:47 +0300

    day02 part2

--- app/2021/day02/run.sql
+++ app/2021/day02/run.sql
@@ -15,7 +15,7 @@ with
       case
         when movement_direction = 'forward' then movement_distance
         else 0
-        end as horizontal,
+      end as horizontal,
       case
         when movement_direction = 'up' then -movement_distance
         when movement_direction = 'down' then movement_distance
@@ -26,5 +26,40 @@ with
   )
 select
   format('Part 1 answer is: %s', sum(horizontal) * sum(vertical))
+from
+  deltas;
+
+with
+  deltas as (
+    select
+      *,
+      case
+        when movement_direction = 'forward' then movement_distance
+        else 0
+        end as horizontal,
+      case
+        when movement_direction = 'forward' then movement_distance * a.aim_sum
+        else 0
+        end as vertical
+    from
+      day02.input i
+        cross join lateral (
+        select
+          sum(case
+                when movement_direction = 'up' then -movement_distance
+                when movement_direction = 'down' then movement_distance
+                else 0
+            end) as aim_sum
+        from
+          day02.input _i
+        where
+            _i.movement_id <= i.movement_id
+        ) a
+    order by
+      movement_id
+  )
+select
+  format('Part 2 answer is: %s', sum(horizontal) * sum(vertical))
+
 from
   deltas
